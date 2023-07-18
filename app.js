5e47a2f7e87edc86689fa2e149124c8d57b94e7b
8288tom <tomshkd1@gmail.com>
2023-07-18 22:29:04 +1000

    updated env port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// if (process.env.NODE_ENV !== "production") {
-//     require("dotenv").config()
-// }
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
 
-// require("dotenv").config()
+require("dotenv").config()
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
@@ -154,6 +154,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err })
 })
 
-app.listen(3000, () => {
-    console.log("Serving on port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
